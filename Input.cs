d9e0a887ce5dbf776f126d4f296eb700b5f58b80
MJPCrepin <manuel@crep.in>
2017-11-18 22:15:31 +0000

    Initial commit

---
+++ Input.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace aStarMazeSolver
+{
+    static class Input
+    {
+        private static int mazeWidth, mazeHeight, startX, startY, endX, endY;
+
+        public static void getInputs()
+        {
+            getMazeSize();
+            getStartPoint();
+            getEndPoint();
+
+            // For testing purposes
+            //Console.WriteLine("mazeWidth=" + mazeWidth);
+            //Console.WriteLine("mazeHeight=" + mazeHeight);
+            //Console.WriteLine("startX=" + startX);
+            //Console.WriteLine("startY=" + startY);
+            //Console.WriteLine("endX=" + endX);
+            //Console.WriteLine("endY=" + endY);
+
+            Console.Read(); // hang
+
+
+            // Build a MazeParser and pass lines
+        }
+
+        private static void getMazeSize()
+        {
+            Console.WriteLine("Please enter maze <WIDTH> and <HEIGHT>");
+
+            string[] tokens = Console.ReadLine().Split();
+            try
+            {
+                mazeWidth = int.Parse(tokens[0]);
+                mazeHeight = int.Parse(tokens[1]);
+            }
+            catch
+            {
+                Console.WriteLine("Invalid input, try again (must be in format <INT> <INT>)");
+                getMazeSize();
+            }
+        }
+
+        private static void getStartPoint()
+        {
+            Console.WriteLine("Please enter <START_X> and <START_Y>");
+
+            string[] tokens = Console.ReadLine().Split();
+            try
+            {
+                startX = int.Parse(tokens[0]);
+                startY = int.Parse(tokens[1]);
+            }
+            catch
+            {
+                Console.WriteLine("Invalid input, try again (must be in format <INT> <INT>)");
+                getStartPoint();
+            }
+        }
+
+        private static void getEndPoint()
+        {
+            Console.WriteLine("Please enter <END_X> and <END_Y>");
+
+            string[] tokens = Console.ReadLine().Split();
+            try
+            {
+                endX = int.Parse(tokens[0]);
+                endY = int.Parse(tokens[1]);
+            }
+            catch
+            {
+                Console.WriteLine("Invalid input, try again (must be in format <INT> <INT>)");
+                getEndPoint();
+            }
+        }
+    }
+}
---
+++ Node.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace aStarMazeSolver
+{
+    // Object used to hold information about each valid grid space
+    class Node
+    {
+        private int x, y;
+        Node parent { get; set; }
+        private int f { get; set; } // (g+h)
+        private int g { get; set; } // movement cost (from start)
+        private int h { get; set; } // heuristic (estimated distance to end)
+
+        public Node(int x, int y)
+        {
+            this.x = x;
+            this.y = y;
+            parent = null;
+        }
+
+        public Node(int x, int y, int h)
+        {
+            this.x = x;
+            this.y = y;
+            parent = null;
+            this.h = h;
+        }
+    }
+}
